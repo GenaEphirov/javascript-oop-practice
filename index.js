93bf9a7156f5e6a811fccda5efa7770d4edc47a3
GenaEphirov <self.education.1411@gmail.com>
2022-07-31 15:14:16 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-// Import stylesheets
-import './style.css';
+'use strict'
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function student() {
+  this.presence = true;
+  this.age = 17;
+  this.marks = [5, 5, 5, 5, 3, 5, 3, 4, 4, 4, 3];
+  this.average = (iter) => {
+    if (!iter.length) {
+      return `no info`
+    }
+    return iter.reduce((a, b) => a+b) / iter.length;
+  };
+}
+
+function worker() {
+  this.education = 'МГУ';
+  this.place = 'office';
+  this.salary = 50000
+  this.getInfo = () => {
+    return this
+  }
+}
+
+worker.prototype.__proto__ = new student()
+
+let Andrey = new student('Andrey');
+Andrey.marks = [];
+let AndreyAfter = new worker();
+
+let someObj = {}
+
