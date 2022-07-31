0b040dbf2c8a02eaaeaa2da6ec4923d9d9e7e968
GenaEphirov <self.education.1411@gmail.com>
2022-07-31 16:13:17 +0300

    1 file modified

--- index.js
+++ index.js
@@ -27,5 +27,48 @@ let Andrey = new student('Andrey');
 Andrey.marks = [];
 let AndreyAfter = new worker();
 
-let someObj = {}
+let parent = {
+  string: 'some string',
+  array: [1, 2, 3, 4, 5],
+  object: {
+    changable: 'it will be changed',
+    unchangable: 'constant'
+  }
+}
+let child = Object.create(parent, {
+  name: {value: 'just a child'},
+})
+let clone = Object.create(Object.getPrototypeOf(child), Object.getOwnPropertyDescriptors(child))
 
+parent.object.changable = 'Changed'
+
+let createFruit = function(kind, price) {
+  return class {
+    constructor() {
+      this.kind = kind;
+      this.price = price;
+    }
+    getPrice() {
+      return `${kind} costs ${price}`
+    }
+  }
+}
+let fApple = createFruit('apple', 100)
+let apple = new fApple
+
+class Book {
+  constructor(title, author, price) {
+    this.title = title;
+    this.author = author;
+    this.price = price;
+  }
+  get bookTitle() {return this.title}
+  set bookTitle(value) {this.title = value}
+}
+
+let myFavBook = new Book('Мастер и Маргарита', 'М.А.Булгаков', 2000)
+console.log(myFavBook.title)
+console.log(myFavBook.bookTitle)
+myFavBook.bookTitle = 'Белая гвардия'
+console.log(myFavBook.title)
+console.log(myFavBook.bookTitle)
